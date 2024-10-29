5616491f80f4a979624b387b0de2ae537090d3a4
sibarchannayak <sibarchannayak2017@gmail.com>
2024-10-30 01:52:08 +0530

    change in auto login

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -5,15 +5,9 @@ import Login from "../pages/user/login/Login";
 import SignUp from "../pages/user/signUp/SignUp";
 import Home from "../pages/user/home/Home";
 import Spinner from "../components/spinner/Spinner";
-import useAutoLogin from "../hooks/useAutoLogin";
 
 const UserRoutes = () => {
   const isAuth = useSelector((state) => state.user.auth);
-  const loading = useAutoLogin();
-
-  if (loading) {
-    return <Spinner />;
-  }
 
   return (
     <Routes>
